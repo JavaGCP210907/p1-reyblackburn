9a3cade6371cf33f0e83194dc6f1289121189bca
Rey Blackburn <rey.blackburn@revature.net>
2021-10-07 16:46:12 -0600

    Updated testing

--- src/test/java/com/revature/Tests.java
+++ src/test/java/com/revature/Tests.java
@@ -2,6 +2,7 @@ package com.revature;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Connection;
@@ -11,12 +12,9 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.revature.dao.ReimbursementDao;
-import com.revature.dao.UsersDao;
 import com.revature.models.Reimbursement;
 import com.revature.services.LoginServices;
 import com.revature.services.ReimbursementService;
-import com.revature.utils.ConnectionUtil;
 
 public class Tests {
 	
@@ -35,8 +33,33 @@ public class Tests {
 		rs = new ReimbursementService();
 	}
 	
+	@Test
+	public void CheckPasswordValidUserAndPasswordReturnTrue() {
+		boolean result = ls.login(usern, userp);
+		assertTrue(result);
+	}
 	
+	@Test
+	public void CheckPasswordInvalidUserAndPasswordReturnFalse() {
+		boolean result = ls.login(usernFake, userpFake);
+		assertFalse(result);
+	}
 	
+	@Test
+	public void CheckRoleIdValue1() {
+		int result = ls.getRoleId(usern, userp);
+		assertEquals(1, result);
+	}
 	
+	@Test
+	public void CheckUserIdValue1() {
+		int result = ls.getUserId(usern, userp);
+		assertEquals(1,result);
+	}
+	
+	@Test
+	public void CheckUpdateReimbursementThrowsSQLException() {
+		assertThrows(SQLException.class, () -> rs.updateReimbursementStatus(80, 'd', 'v'));
+	}
 
 }
