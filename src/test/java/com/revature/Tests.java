9754959fdfa78e7282af11b0eb67691109331055
Rey Blackburn <rey.blackburn@revature.net>
2021-10-08 15:09:27 -0600

    Updated Testing to have more tests

--- src/test/java/com/revature/Tests.java
+++ src/test/java/com/revature/Tests.java
@@ -2,17 +2,12 @@ package com.revature;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.revature.models.Reimbursement;
 import com.revature.services.LoginServices;
 import com.revature.services.ReimbursementService;
 
@@ -46,9 +41,27 @@ public class Tests {
 	}
 	
 	@Test
+	public void CheckPasswordInvalidUserAndPasswordNotNull() {
+		boolean result = ls.login(usernFake, userpFake);
+		assertNotNull(result);
+	}
+	
+	@Test
+	public void CheckRoleIdValueNotNull() {
+		int result = ls.getRoleId(usern, userp);
+		assertNotNull(result);
+	}
+	
+	@Test
 	public void CheckRoleIdValue1() {
 		int result = ls.getRoleId(usern, userp);
 		assertEquals(1, result);
+	}
+	
+	@Test
+	public void CheckUserIdValueNotNull() {
+		int result = ls.getUserId(usern, userp);
+		assertNotNull(result);
 	}
 	
 	@Test
@@ -58,8 +71,20 @@ public class Tests {
 	}
 	
 	@Test
-	public void CheckUpdateReimbursementThrowsSQLException() {
-		assertThrows(SQLException.class, () -> rs.updateReimbursementStatus(80, 'd', 'v'));
+	public void CheckGetAllReimbursementsNotNull() {
+		assertNotNull(rs.getAllReimbursements());
 	}
+	
+	@Test
+	public void CheckGetReimbursementByStatusNotNull() {
+		assertNotNull(rs.getReimbursementByStatus(1));
+	}
+	
+	@Test
+	public void CheckGetReimbursementByUserIdNotNull() {
+		assertNotNull(rs.getReimbursementsByUserId(2));
+	}
+	
+	
 
 }
